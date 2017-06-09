f1f63b8096b1759ba00dd6df8364d5c686a638f8
jerem-nte <chloefriang@imac-de-chloe.home>
2017-06-09 19:18:40 +0200

    CHF as first currency

--- src/main/resources/config/insert_users_currencies.sql
+++ src/main/resources/config/insert_users_currencies.sql
@@ -1,5 +1,5 @@
 MERGE INTO users(id, name, gender) VALUES(1, 'Jeremy', 'm');
 MERGE INTO users(id, name, gender) VALUES(2, 'Chloe', 'f');
 
-MERGE INTO currency(id, name, shortname, icon) VALUES(1, 'Euro', 'EUR', 'no');
-MERGE INTO currency(id, name, shortname, icon) VALUES(2, 'Franc suisse', 'CHF', 'no');
+MERGE INTO currency(id, name, shortname, icon) VALUES(1, 'Franc suisse', 'CHF', 'no');
+MERGE INTO currency(id, name, shortname, icon) VALUES(2, 'Euro', 'EUR', 'no');
